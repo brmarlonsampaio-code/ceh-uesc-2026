27623650ab9bb3c9a87a4587d5a919b7b6ed15b2
brmarlonsampaio-code <br.marlon.sampaio@gmail.com>
2026-08-10 14:22:46 -0300

    Fix blank screen on GitHub Pages using HashRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
